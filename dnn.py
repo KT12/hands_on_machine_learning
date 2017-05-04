48f0df69ad4ec38f342805b688ad46be52132ce8
Ken Tsuji <nyy2008@gmail.com>
2017-05-04 11:13:29 -0400

    Add code for DNN and MNIST
    DNN runs MNIST classification on GNN properly.

--- dnn.py
+++ dnn.py
@@ -1,6 +1,7 @@
 # Train a DNN using TF
 
 import tensorflow as tf
+from tensorflow.contrib.layers import fully_connected
 
 n_inputs = 28* 28
 n_hidden_1 = 300
@@ -22,3 +23,51 @@ def neuron_layer(X, n_neurons, name, activation=None):
             return tf.nn.relu(z)
         else:
             return z
+
+# with tf.name_scope('DNN'):
+#     hidden_1 = neuron_layer(X, n_hidden_1, 'Hidden_1', activation='relu')
+#     hidden_2 = neuron_layer(hidden_1, n_hidden_2, 'Hidden_2', activation='relu')
+#     logits = neuron_layer(hidden_2, n_outputs, 'Outputs')
+
+# Use fully_connected instead
+with tf.name_scope('DNN'):
+    hidden_1 = fully_connected(X, n_hidden_1, scope='Hidden_1')
+    hidden_2 = fully_connected(hidden_1, n_hidden_2, scope='Hidden_2')
+    logits = fully_connected(hidden_2, n_outputs, scope='Outputs', activation_fn=None)
+
+with tf.name_scope('loss'):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(xentropy, name='loss')
+
+learning_rate = 0.01
+
+with tf.name_scope('train'):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+
+with tf.name_scope('eval'):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+# Execution phase
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/")
+
+n_epochs = 400
+batch_size = 50
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for iteration in range(mnist.train.num_examples // batch_size):
+            X_batch, y_batch = mnist.train.next_batch(batch_size)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+        acc_test = accuracy.eval(feed_dict={X: mnist.test.images, y: mnist.test.labels})
+        print(epoch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
+
+    save_path = saver.save(sess, "./my_model_final.ckpt")
